7fae126e3442e468e348dac238881f3eb2ae1cc9
Emery Chikezie Ogechukwu <emerychikezie2@gmail.com>
2024-03-18 01:48:03 +0100

    loaded some CSS components

--- services/Router.js
+++ services/Router.js
@@ -8,6 +8,11 @@ const Router = {
         Router.go(url);
       });
     });
+    // Event handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
     // check the initial URL
     Router.go(location.pathname);
   },
@@ -17,6 +22,28 @@ const Router = {
 
     if (addToHistory) {
       history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("menu-page");
+        break;
+      case "/order":
+        pageElement = document.createElement("order-page");
+        break;
+      default:
+        if (route.startWith("/product-")) {
+          pageElement = document.createElement("details-page");
+          const paramId = route.subtring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+    if (pageElement) {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
     }
   },
 };
